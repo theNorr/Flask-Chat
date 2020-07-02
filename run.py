0ea4fdce2ff2dbe36ac415aa7380de7bc166d6d9
The-element <nicke.jonsson@live.com>
2020-07-02 10:42:14 +0000

    finished chat project

--- run.py
+++ run.py
@@ -9,7 +9,8 @@ messages = []  # a list to store our messages.
 def add_messages(username, message):   # The method used to actually store the messages.
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")  # strftime takes the datetime.now and convert it to a string with the given format of hour:minute:second
-    messages.append("({}) {}: {}".format(now, username, message))
+    messages_dict = {"timestamp": now, "from": username, "message": message}  # A dictionary(or object in javascript).
+    messages.append(messages_dict)  # appends above message to the messages list.
 
 def get_all_messages():  # The metod to collect and add all messages.
     """Get all of the messages and separate them with a `br`"""
@@ -29,16 +30,16 @@ def index():
 
 
 
-@app.route("/<username>")  # Whatever is inside of the anglebrackets is a variable.
+@app.route("/<username>", methods=["GET", "POST"])  # Whatever is inside of the anglebrackets is a variable.
 def user(username):  # user will take the argument of username.
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages()) # .format()-method adds the username instead of the placeholder, calls on the function get_all_messages
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
 
-
-@app.route("/<username>/<message>")
-def send_message(username, message):
-    """Create a new message and redirect back to the chat page"""
-    add_messages(username, message)  # calls the function add_messages and passes in the username: and message.
-    return redirect("/" + username)  # redirects the user to the url index/username
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
