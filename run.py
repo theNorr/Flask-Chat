a700a59503e8d358641c1ff222907ccb1486df7b
The-element <nicke.jonsson@live.com>
2020-07-02 08:06:14 +0000

    added functionallity

--- run.py
+++ run.py
@@ -2,7 +2,15 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+messages = []  # a list to store our messages.
 
+def add_messages(username, message):   # The method used to actually store the messages.
+    """Add messages to the `messages` list"""
+    messages.append("{}: {}".format(username, message))
+
+def get_all_messages():  # The metod to collect and add all messages.
+    """Get all of the messages and separate them with a `br`"""
+    return "<br>".join(messages)
 
 @app.route("/")
 def index():
@@ -11,13 +19,15 @@ def index():
 
 
 @app.route("/<username>")  # Whatever is inside of the anglebrackets is a variable.
+"""Display chat messages"""
 def user(username):  # user will take the argument of username.
-    return "Hi " + username
+    return "Welcome {0}".format(username, get_all_messages()) # .format()-method adds the username instead of the placeholder, calls on the function get_all_messages
 
 
 @app.route("/<username>/<message>")
+"""Create a new message and redirect back to the chat page"""
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)  # returns the username and the message.
-
+    add_messages(username, message)  # calls the function add_messages and passes in the username: and message.
+    return redirect("/" + username)  # redirects the user to the url index/username
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
