4e07dad806459d1c928308e87fd1677de0db02c8
The-element <nicke.jonsson@live.com>
2020-07-02 07:49:29 +0000

    Implement basic views and routing

--- run.py
+++ run.py
@@ -6,10 +6,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello There</h1>"
+    """Main page with instructions"""
+    return "To send a message use: /USERNAME/MESSAGE"
 
 
-if __name__ =="__main__":
-    app.run(host=os.getenv("IP"),
-       port=int(os.getenv("PORT")),
-       debug=True)
+@app.route("/<username>")  # Whatever is inside of the anglebrackets is a variable.
+def user(username):  # user will take the argument of username.
+    return "Hi " + username
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)  # returns the username and the message.
+
+
+app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
